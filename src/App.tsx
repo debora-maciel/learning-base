d5847dba932cef12aaed8a98de294d2b5c165e08
trxuxv <deboraleticiaferreiramaciel@gmail.com>
2023-12-06 19:24:57 -0300

    feat: logged in function

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,7 @@ function App() {
   return (
     <Provider store={store}>
       <Template>{/* ROUTES */}
-        <></>
+        <>Logged</>
       </Template>
     </Provider>
   );
--- src/auth/pages/Login.tsx
+++ src/auth/pages/Login.tsx
@@ -1,16 +1,55 @@
+import { Button, TextField } from "@mui/material";
+import img from "./../../assets/icon.png";
+import { useAppDispatch } from "../../hooks/useAppDispatch";
+import { signIn } from "../store/user/userSlice";
+import { useState } from "react";
+
 export default function Login() {
+  const dispatch = useAppDispatch();
+  const [user, setUser] = useState<{ user: string }>();
+
+  function onLogin() {
+    if (user)
+      dispatch(
+        signIn({
+          isLoggedIn: true,
+          login: user?.user,
+          session: user?.user,
+        })
+      );
+  }
+
   return (
-    <div className="w-screen bg-slate-100 h-screen flex items-center justify-center">
-      <div className="rounded bg-white shadow-md flex flex-col gap-6 p-2">
+    <div className="w-screen bg-slate-200 h-screen flex items-center justify-center">
+      <div className="rounded w-[300px] h-[420px] bg-white shadow-md flex flex-col gap-6 p-6">
+        <div className="w-full flex items-center justify-center flex-col">
+          <img className=" shadow-lg rounded w-16 bg-blue-500" src={img} />
+          <p className="text-xs mt-2">Learning Base</p>
+          <p className={"font-bold text-xl mt-4"}>Login</p>
+        </div>
         <div className="flex flex-col gap-1">
-          <label>Username</label>
-          <input  className="rounded border p-2"/>
+          <TextField
+            onChange={(ev) => setUser({ user: ev.target.value })}
+            size={"small"}
+            id="outlined-basic"
+            label="Username"
+            variant="outlined"
+          />
         </div>
-        <div>
-            <label>Password </label>
-          <input />
+        <div className="flex flex-col gap-1">
+          <TextField
+            size={"small"}
+            type="password"
+            id="outlined-basic"
+            label="Password"
+            variant="outlined"
+          />
         </div>
-        <input />
+        <div className="w-full flex items-center justify-center">
+          <Button onClick={onLogin} fullWidth variant="contained">
+            SignIn
+          </Button>
+        </div>
       </div>
     </div>
   );
